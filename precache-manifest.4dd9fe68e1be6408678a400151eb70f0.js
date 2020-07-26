69cddfa1493cbaddf4ea7dc9bcdecc02fc02e19a
gonramos <goncalo.rms@gmail.com>
2020-07-26 16:18:38 +0200

    Updates

---
+++ precache-manifest.4dd9fe68e1be6408678a400151eb70f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6ad7752051c6eb4b2daafd86b9b8241",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "9deb9df0c9e41a547365",
+    "url": "/todo-list/static/js/2.2c403b61.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todo-list/static/js/2.2c403b61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecedcc480b97fb38885a",
+    "url": "/todo-list/static/js/main.cf8e8a22.chunk.js"
+  },
+  {
+    "revision": "4c207b602d40809001a5",
+    "url": "/todo-list/static/js/runtime-main.2929926a.js"
+  }
+]);
